bb69120dd7c2a9cc9053c91e9d2de84ada7f1337
lejubi <113481201+lejubi@users.noreply.github.com>
2025-09-07 22:14:28 -0400

    fixed swerve drive stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/SwerveDriveOpMode.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import static java.lang.Thread.sleep;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.utilities.CONFIG;
+import org.firstinspires.ftc.teamcode.utilities.ClawRotator;
+import org.firstinspires.ftc.teamcode.utilities.Slides;
+import org.firstinspires.ftc.teamcode.utilities.Claw;
+import org.firstinspires.ftc.teamcode.utilities.PullUp;
+import org.firstinspires.ftc.teamcode.utilities.SimpleMecanumDrive;
+import org.firstinspires.ftc.teamcode.utilities.SlidesState;
+import org.firstinspires.ftc.teamcode.utilities.SwerveDrive;
+
+@Config
+@TeleOp(name="Swerve Drive")
+public class SwerveDriveOpMode extends OpMode {
+
+    SwerveDrive drive;
+
+    float stick_margin = 0.1f;
+    final double normalPower = 0.9;
+
+    public void init() {
+        this.drive = new SwerveDrive(hardwareMap);
+    }
+
+    public void loop() {
+
+        //DRIVE
+        float x = gamepad2.left_stick_x;
+        float y = gamepad2.left_stick_y;
+        float turn = gamepad2.right_stick_x;
+        move(x, y, turn);
+
+    }
+
+    public void move(float x, float y, float turn) {
+        // if the stick movement is negligible, set STICK_MARGIN to 0
+
+        if (Math.abs(x) <= stick_margin) x = .0f;
+        if (Math.abs(y) <= stick_margin) y = .0f;
+        if (Math.abs(turn) <= stick_margin) turn = .0f;
+
+        //Notation of a ? b : c means if a is true do b, else do c.
+        double multiplier = normalPower;
+        drive.move(-x * multiplier, y * multiplier, -turn * multiplier);
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/CONFIG.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/CONFIG.java
@@ -5,10 +5,15 @@ import com.bylazar.configurables.annotations.Configurable;
 @Configurable
 public class CONFIG {
     //drivetrain wheels
-//    public static String FRONT_RIGHT = "fr"; // 0
-//    public static String BACK_RIGHT = "br"; // 1
-//    public static String BACK_LEFT = "bl"; // 2
-//    public static String FRONT_LEFT = "fl"; // 3
+    public static String FRONT_RIGHT = "fr"; // 0
+    public static String BACK_RIGHT = "br"; // 1
+    public static String BACK_LEFT = "bl"; // 2
+    public static String FRONT_LEFT = "fl"; // 3
+
+    public static String FR_STEER = "fr"; // 0
+    public static String BR_STEER = "br"; // 1
+    public static String BL_STEER = "bl"; // 2
+    public static String FL_STEER = "fl"; // 3
 
     //public static String intake = "in";
     //public static String rotator = "rot";
