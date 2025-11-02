50235783c248957dbfb5f1171cfbafcb8a840e9c
lejubi <113481201+lejubi@users.noreply.github.com>
2025-11-02 01:56:08 -0500

    justmove auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/jsutMove.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/jsutMove.java
@@ -1,12 +1,13 @@
 package org.firstinspires.ftc.teamcode.auton;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.utilities.Intake;
 import org.firstinspires.ftc.teamcode.utilities.Rotator;
 import org.firstinspires.ftc.teamcode.utilities.SwerveDrive;
 
-@Autonomous(name = "auton_justMove")
+@Autonomous(name = "time_justMove", group = "Linear OpMode")
 public class jsutMove extends AutonMethods {
 
     @Override
@@ -20,10 +21,12 @@ public class jsutMove extends AutonMethods {
         telemetry.update();
 
         // ✅ Wait for the game to start
+        ElapsedTime runtime = new ElapsedTime();
         waitForStart();
+        runtime.reset();
 
         // ✅ Now run movements
-        moveForward(10.0);
+        moveForward(1.0);
     }
 }
 
