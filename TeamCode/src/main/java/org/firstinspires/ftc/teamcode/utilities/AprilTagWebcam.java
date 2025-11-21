aafdfe566c1db5fa3390647316687ee224593f80
Puhalenthi Ramesh Vidhya <57294477+Puhalenthi@users.noreply.github.com>
2025-11-21 16:55:40 -0500

    update AprilTagWebcam for new image format
    We previously used YUY2 which is unsupported on our camera. The fix now uses MJPEG

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/AprilTagWebcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/AprilTagWebcam.java
@@ -40,6 +40,7 @@ public class AprilTagWebcam {
         VisionPortal.Builder builder = new VisionPortal.Builder();
         builder.setCamera(hwMap.get(WebcamName.class, "Webcam 1"));
         builder.setCameraResolution(new Size(1920, 1080));
+        builder.setStreamFormat(VisionPortal.StreamFormat.MJPEG);
         builder.addProcessor(aprilTagProcessor);
 
         visionPortal = builder.build();
