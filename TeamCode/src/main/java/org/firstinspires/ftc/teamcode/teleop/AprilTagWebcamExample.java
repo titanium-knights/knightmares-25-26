800673ef4eaecde9031cf91a7e1432add2a34140
Puhalenthi Ramesh Vidhya <57294477+Puhalenthi@users.noreply.github.com>
2025-11-21 17:59:25 -0500

    add telemetry for camera

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/AprilTagWebcamExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/AprilTagWebcamExample.java
@@ -42,7 +42,7 @@ public class AprilTagWebcamExample extends OpMode {
     public void loop() {
         aprilTagWebcam.update();
         AprilTagDetection id20 = aprilTagWebcam.getTagBySpecificId(20);
-        // telemetry.addData("id20 String", id20.toString());
+        telemetry.addData("id20 String", id20.toString());
 
     }
 }
